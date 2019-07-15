e4e1789a7705996246335d66a27e24c661448dfe
mwinb <mwinb>
2019-07-14 22:19:42 -0700

    Added hubble images

--- js/apoidRequest.js
+++ js/apoidRequest.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
   var url =
     "https://api.nasa.gov/planetary/apod?api_key=x6vBGH5z5njOqWKVMz5pcXKnBPCYDDgGMeQaGcPl";
 
+  var hubbleImagesUrl = "https://hubblesite.org/api/v3/images";
+
   $.ajax({
     url: url,
-    success: function(result) {
+    error: () => {
+      $("#apoidLI").remove();
+    },
+    success: result => {
       if ("copyright" in result) {
         $("#apoidCopyWrite").text("Image Credits: " + result.copyright);
       } else {
@@ -20,6 +25,48 @@ $(document).ready(function() {
       }
       $("#apoidDescription").text(result.explanation);
       $("#apoidTitle").text(result.title);
-    }
+
+      getHubbleImages();
+    },
+    timeout: 3000
   });
+
+  getHubbleImages = () => {
+    $.ajax({
+      url: hubbleImagesUrl,
+      type: "GET",
+      dataType: "jsonp",
+      success: function(result) {
+        result.forEach(element => {
+          getImageByID(element.id);
+        });
+      }
+    });
+  };
+
+  getImageByID = imageID => {
+    $.ajax({
+      url: `https://hubblesite.org/api/v3/image/${imageID}`,
+      type: "GET",
+      dataType: "jsonp",
+      success: function(result) {
+        console.log(result.image_files.file_url);
+        $("#slideContainer").append(
+          `<li>
+        <img src="https:${
+          result.image_files[0].file_url.includes("png")
+            ? result.image_files[0].file_url
+            : result.image_files[1].file_url
+        }" alt="" uk-cover>
+        <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+          <h3 class="uk-margin-remove">${result.name}</h3>
+          <p class="uk-margin-remove">${result.mission}</p>
+          <p class='uk-margin-remove'>${result.description && result.description.length < 5000 ? 
+            result.description : result.collection}</p>
+        </div>
+      </li>`
+        );
+      }
+    });
+  };
 });
--- js/hubbleNewsRequest.js
+++ js/hubbleNewsRequest.js
@@ -2,66 +2,58 @@ $(document).ready(function() {
   const articleListURL = "https://hubblesite.org/api/v3/news?page=all";
   let idArray = [];
   let totalShown = 0;
-  
-  $.ajax({
-    url: articleListURL,
-    type: 'GET',
-    dataType: 'jsonp',
-    success: function(result) {
-      count = 0;
-      while(count < 25)
-      {
-      
-        getArticleByID(result[count].news_id, count);
-        count++;
-        totalShown++;
+
+  getAllArticles = () => {
+    $.ajax({
+      url: articleListURL,
+      type: "GET",
+      dataType: "jsonp",
+      success: function(result) {
+        result.forEach(element => {
+          idArray.push(element.news_id);
+        });
+        let startCount = 0;
+        totalShown += 25;
+        while (startCount < totalShown) {
+          getArticleByID(result[startCount].news_id, startCount);
+          startCount++;
+        }
       }
-      result.forEach(element => {
-        idArray.push(element.news_id);
-      });
-    }
-  });
+    });
+  };
 
   getArticleByID = (articleID, count) => {
     $.ajax({
       url: `https://hubblesite.org/api/v3/news_release/${articleID}`,
-      dataType: 'jsonp',
+      dataType: "jsonp",
       success: function(result) {
-        const alignRight =
-          "articleImage uk-flex-last@s uk-card-media-right uk-cover-container";
         const alignLeft = "articleImage uk-card-media-left uk-cover-container";
-          $("#hubbleNewsFeedContainer").append(
-            `<div class="spaceFlightCardContainer uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
-                <div class="${alignRight}">
-                    <img src="https:${result.thumbnail_retina}" alt="" uk-cover>
-                    <canvas width="200" height="200"></canvas>
+        $("#hubbleNewsFeedContainer").append(
+          `<div class="spaceFlightCardContainer uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+              <div class="${alignLeft}">
+                  <img src="https:${result.thumbnail_retina}" alt="" uk-cover>
+                  <canvas width="200" height="200"></canvas>
+              </div>
+              <div id='articleContent'>
+                <div class="uk-card-body">
+                  <h3 class="articleTitle uk-card-title">${result.name}</h3>
+                  <p>${result.abstract}</p>
+                  <a class="uk-link-muted" href="${result.url}">View Article</a>
                 </div>
-                <div id='articleContent'>
-                  <div class="uk-card-body">
-                    <h3 class="articleTitle uk-card-title">${result.name}</h3>
-                    <p>${result.abstract}</p>
-                    <a class="uk-link-muted" href="${
-                      result.url
-                    }">View Article</a>
-                  </div>
-                </div>
-            </div>`
-          );
+              </div>
+          </div>`
+        );
       }
     });
   };
 
   loadMore = () => {
-    let startCount = totalShown;
-    totalShown += 25;
-    while(startCount <= totalShown) {
-      getArticleByID(idArray[startCount]);
-      startCount++;
-    }
+    getAllArticles();
   };
 
-  $('#showMore').click(() => {
+  $("#showMore").click(() => {
     loadMore();
   });
-  
+
+  loadMore();
 });
